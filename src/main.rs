b9bd1482139a1395e3cf8dd852c15295295c6064
Hugo Dutka <contact@hugodutka.com>
2020-10-24 21:12:04 +0200

    initialize before parsing args

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ use crate::core::init::init;
 use clap::Clap;
 
 fn main() {
-    let root = Root::parse();
-
     if let Err(err) = init() {
         eprintln!("Error: {:?}", err);
         std::process::exit(1);
     }
+
+    let root = Root::parse();
 
     if let Err(err) = invoke_action(root) {
         eprintln!("Error: {:?}", err);

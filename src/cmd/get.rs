e9736bc2cc51906c12d2f93688e933dfe458bb58
Hugo Dutka <contact@hugodutka.com>
2020-10-25 22:12:21 +0100

    bump version, fix grammar in docs

--- src/cmd/get.rs
+++ src/cmd/get.rs
@@ -2,7 +2,7 @@ use clap::Clap;
 
 #[derive(Clap)]
 pub struct GetCmd {
-    /// Git URL of the hocus project.
+    /// Git URL of a hocus project.
     pub url: String,
     /// How the project should be named locally.
     pub name: String,

83e099337bb0c26a876839640658c279ba6c25ac
Hugo Dutka <contact@hugodutka.com>
2020-10-29 19:19:17 +0100

    fix a bug in get_docker_container_name

--- src/core/project.rs
+++ src/core/project.rs
@@ -3,5 +3,6 @@ pub fn get_project_docker_prefix(project_name: &str) -> String {
 }
 
 pub fn get_docker_container_name(project_name: &str, service_name: &str) -> String {
-    format!("{}_{}_1", project_name, service_name)
+    let prefix = get_project_docker_prefix(project_name);
+    format!("{}_{}_1", prefix, service_name)
 }
